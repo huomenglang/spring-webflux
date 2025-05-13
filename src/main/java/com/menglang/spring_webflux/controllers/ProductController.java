0c1f8be17ac891e8ba5df480d20f6fec08b2ca5d
Menglang6969 <huomenglang@gmail.com>
2025-05-13 13:58:52 +0700

    add mathservic & exceptionHandler

--- src/main/java/com/menglang/spring_webflux/controllers/ProductController.java
+++ src/main/java/com/menglang/spring_webflux/controllers/ProductController.java
@@ -1,2 +1,24 @@
-package com.menglang.spring_webflux.controllers;public class ProductController {
+package com.menglang.spring_webflux.controllers;
+
+import com.menglang.spring_webflux.dtos.MultiplyRequestDTO;
+import com.menglang.spring_webflux.dtos.response.ResponseDTO;
+import com.menglang.spring_webflux.services.product.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+//    @GetMapping(value = "table/{input}",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+//    public Flux<ResponseDTO> table(@PathVariable int input){
+//        return productService.getProduct();
+//    }
+
 }
--- src/main/java/com/menglang/spring_webflux/services/product/ProductService.java
+++ src/main/java/com/menglang/spring_webflux/services/product/ProductService.java
@@ -1,2 +1,29 @@
-package com.menglang.spring_webflux.services.product;public class ProductService {
+package com.menglang.spring_webflux.services.product;
+
+
+import com.menglang.spring_webflux.dtos.product.Product;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Service
+@Slf4j
+public class ProductService {
+
+    public Flux<Product> getProduct(){
+        return Flux.range(1, 10)
+                .delayElements(Duration.ofSeconds(1))
+                .map(x -> new Product(x, "product_"+x, x));
+    }
+
+    public Flux<Product> getProductWithError(){
+        return Flux.range(1,10)
+                .delayElements(Duration.ofSeconds(1))
+                .take(4)
+                .map(x->new Product(x, "product_"+x, x))
+                .concatWith(Mono.fromRunnable(()->System.exit(1)));
+    }
 }
